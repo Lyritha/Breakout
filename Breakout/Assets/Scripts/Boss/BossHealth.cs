7f11c25731a1cd386c98298ebf29f9559087922b
AsianScammer <dennis29012006@gmail.com>
2026-06-04 14:24:26 +0200

    Feature Boss health
    Er is een baas in het spel met een healthbar dat visueel zichtbaar is voor de speler.
    
    Ook kan de boss geraakt worden door een ball waarbij er ook een visueel laat zien dat hij geraakt wordt.

---
+++ Breakout/Assets/Scripts/Boss/BossHealth.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour
+{
+    private int currentHealth = 20;
+    private int maxHealth = 20;
+    [SerializeField] private Image healthBarFill;
+    
+    //Make the boss color change to red by impact
+    private SpriteRenderer bossSpriteRenderer;
+
+
+    private void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBarFill.fillAmount = (float)currentHealth / maxHealth;
+    }
+    
+    //Make the boss color change to red by impact method
+    private void FlashRed()
+    {
+        if (bossSpriteRenderer == null)
+        {
+            bossSpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        
+        if (bossSpriteRenderer != null)
+        {
+            StartCoroutine(FlashRedCoroutine());
+        }
+    }
+
+    private IEnumerator FlashRedCoroutine()
+    {
+        Color originalColor = bossSpriteRenderer.color;
+        bossSpriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        bossSpriteRenderer.color = originalColor;
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        FlashRed();
+        UpdateHealthBar();
+        Die();
+    }
+    
+    private void Die()
+    {
+        if (currentHealth <= 0)
+        {
+            GameEvents.onGameWon?.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.TryGetComponent<BallBounce>(out var ball))
+        {
+            TakeDamage(1);
+        }
+    }
+}
