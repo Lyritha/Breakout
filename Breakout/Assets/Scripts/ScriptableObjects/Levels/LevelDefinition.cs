c42f359c7ef0db77d79ba9c0e3670a173c971c7c
Lyritha <Collin.rarekip@gmail.com>
2026-06-10 20:58:38 +0200

    added background music and new levels

--- Breakout/Assets/Scripts/ScriptableObjects/Levels/LevelDefinition.cs
+++ Breakout/Assets/Scripts/ScriptableObjects/Levels/LevelDefinition.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Levels/Level Definition")]
 public class LevelDefinition : ScriptableObject
 {
+    public LevelType levelType = LevelType.Default;
+
     public string levelName = "New Level";
     public Color levelColor = Color.white;
 
     public LevelRow[] rows = new LevelRow[10];
 }
 
+public enum LevelType
+{
+    Default,
+    Boss
+}
