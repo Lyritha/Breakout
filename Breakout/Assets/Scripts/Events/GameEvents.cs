cf874e0e9420ea5b78ed5c5db4fda0f98b0157a1
roni702 <152854987+roni702@users.noreply.github.com>
2026-06-10 20:34:03 +0200

    feat: verbeter bal physics en paddle interactie (US-04)
    - Bal stuitert correct van bovenkant paddle met hoekberekening
    - Zijkant paddle duwt bal omhoog weg
    - Onderkant paddle triggert onBallLost en reset bal boven paddle
    - Snelheid neemt toe per botsing, reset bij paddle bovenkant
    - ClampMinY voorkomt horizontaal vastzitten

--- Breakout/Assets/Scripts/Events/GameEvents.cs
+++ Breakout/Assets/Scripts/Events/GameEvents.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public static class GameEvents
 {
     public static Action onBallLost;
-
+    public static Action onBallReset;
     public static Action onGameWon;
     public static Action onGameOver;
 }
